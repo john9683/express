cfe94930195f03ca65233868b8fac712efc56917
john9683 <john_9683@mail.ru>
2024-08-05 20:16:52 +0300

    little refactoring

--- NameApi.php
+++ NameApi.php
@@ -1,52 +1,61 @@
 <?php
 
-namespace NamePlugin;
+namespace GetVacancyPlugin;
 
-class NameApi {
+use phpDocumentor\Reflection\Types\Object_;
+
+class GetVacancyApi {
   /**
    * @var string
    */
   public $apiUrl;
 
   /**
-   * @param string $post
    * @param int $vid
-   * @return array|false|mixed
+   * @return ?Object_
    */
-  public function getListVacancies(string $post, int $vid = 0): ?array {
+  public function getVacancy(int $vid = 0): ?Object_ {
 
-    if (!is_object($post)) {
-      return false;
-    }
+    $page = 0;
+
+    do {
+      $res = $this->getRes($page);
+      $res_o = json_decode($res);
+
+      if ($res !== false && is_object($res_o) && isset($res_o->objects)) {
+        foreach ($res_o->objects as $key => $value) {
+          if ($value->id === $vid) {
+            return $value;
+          }
+        }
+      }
+
+      $page++;
+    } while ($res_o->more);
+
+    return null;
+  }
+
+  /**
+   * @return array|null
+   */
+  public function getVacanciesList(): ?array {
 
     $ret = [];
     $page = 0;
 
     do {
-      $params = "status=all&id_user="
-                . $this->selfGetOption('superjob_user_id')
-                . "&with_new_response=0&order_field=date&order_direction=desc&page={$page}&count=100";
-      $res = $this->apiSend($this->apiUrl . '/hr/vacancies/?' . $params);
+      $res = $this->getRes($page);
       $res_o = json_decode($res);
 
       if ($res !== false && is_object($res_o) && isset($res_o->objects)) {
-
-        if ($vid > 0) {
-          foreach ($res_o->objects as $key => $value) {
-            if ($value->id == $vid) {
-
-              return $value;
-            }
-          }
-        }
-
         $ret[] = $res_o->objects;
       }
 
       $page++;
     } while ($res_o->more);
 
-    return count($ret) > 0 ? $ret : false;
+    return count($ret) > 0 ? $ret : null;
   }
 
   /**
@@ -62,5 +71,17 @@ class NameApi {
    */
   public function selfGetOption(string $optionName): string {
     return '';
+  }
+
+  /**
+   * @param $page
+   * @return string
+   */
+  private function getRes($page): string {
+    $params = "status=all&id_user="
+      . $this->selfGetOption('superjob_user_id')
+      . "&with_new_response=0&order_field=date&order_direction=desc&page={$page}&count=100";
+
+    return $this->apiSend($this->apiUrl . '/hr/vacancies/?' . $params);
   }
 }
